16f0f459258bb6d2c59a70bf9172bfc82f939cbd
Bob Oyier <boboyier92@gmail.com>
2022-11-22 22:00:13 +0300

    "completed the more functions lab @ Moringa School"

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code your solution here
+/* The solution can be arrived at in two ways. The submitted answer = by named Function.
+function saturdayFun(activity = "roller-skate") {
+	return `This Saturday, I want to ${activity}!`
+}
+*/
+const saturdayFun = function (activity = "roller-skate") {
+	return `This Saturday, I want to ${activity}!`;
+};
+
+/* The solution can be arrived at in two ways. The submitted answer = by Function Expression.
+	function mondayWork(work = "go to the office") {
+	return `This Monday, I will ${work}.`
+}
+*/
+const mondayWork = function (work = "go to the office") {
+	return `This Monday, I will ${work}.`;
+};
+
+/* console.log(mondayWork());
+*/
+
+function wrapAdjective(symbol = "*") {
+	const howeverYouWant = function (adjective = "special") {
+		return `You are ${symbol}${adjective}${symbol}!`;
+	};
+	return howeverYouWant;
+}
+
+/*wrapAdjective();
+ */
